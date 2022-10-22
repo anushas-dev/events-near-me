0a1b0cbd3ed75ac9e26862390d84a706867875cd
Anusha S <anushasridharan7@gmail.com>
2022-10-22 22:05:49 +0530

    first commit

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,9 +1,9 @@
 import styles from '../styles/components/Avatar.module.css';
-import Image from 'next/image'
+import Image from 'next/image';
 
 const Avatar = ({ src = '', alt = '' }) => (
   <div className={styles.avatar}>
-    {src ? <Image src={src} alt={alt} /> : null}
+    {src ? <Image src={src} alt={alt} height={50} width={50} /> : null}
   </div>
 );
 
